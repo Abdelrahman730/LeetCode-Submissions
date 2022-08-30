28f2e27962bacb6cd7d6814dc298638032ad6df8
Abdelrahman Ahmed <63473052+Abdelrahman730@users.noreply.github.com>
2022-08-30 21:08:36 +0200

    Time: 17 ms (9.80%), Space: 11.5 MB (84.62%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
+    
+    int binarySearch (vector<int>& x, int k)
+    {
+        int l = 0, r = x.size()-1;
+        while (l <= r) {
+            int m = (l+r)/2;
+            
+            if (x[m] == k) 
+                return m;
+            if (x[m] < k) 
+                l = m+1; 
+            else 
+                r = m-1;
+        }
+        return 0;
+    }
+    
     vector<int> targetIndices(vector<int>& nums, int target) {
         
         sort(nums.begin() , nums.end());
         
         vector <int> arr;
         
-        for (int i = 0 ; i < nums.size() ; i++)
+        int index = binarySearch(nums , target);
+        
+        for (int i = index - 1 ; i >= 0 ; i--)
         {
             if (nums[i] == target)
                 arr.push_back(i);
-            else if (nums[i] > target)
+            else
                 break;
         }
+        
+        for (int i = index ; i < nums.size() ; i++)
+        {
+            if (nums[i] == target)
+                arr.push_back(i);
+            else
+                break;
+        }
+        
+        sort(arr.begin() , arr.end());
             
         return arr;
     }
